cdfa2f2c54aec0d40be79382b28425c252ba5c1f
JOEL MONLLAO <joel.monllao01@estudiant.upf.edu>
2024-10-22 18:51:21 +0200

    final add

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,9 +1,31 @@
 ## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
-def main() :
+import sys
+def main():
+	number = int(sys.argv[1])
+	max_divisors = 0
+	divisors_count = 0
+	def count_divisors(n):
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
+		"""Return the number of divisors of n."""
+		count = 0
+		for i in range(1, n + 1):
+			if n % i == 0:
+				count += 1
+		return count
+	divisors_count = count_divisors(number)
+
+	is_anti_prime = True
+	for i in range(1, number):
+		if count_divisors(i) >= divisors_count:
+			is_anti_prime = False
+			break
+	if is_anti_prime:
+		return"antiprime"
+	else:
+		return"not anti-prime"
 
 
 	## THE LAST LINES OF YOUR CODE SHOULD EITHER
